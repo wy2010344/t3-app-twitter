2788cdc508fd09fe8a8480336a0a6a464780c793
wyknmjj <wyknmjj@protonmail.com>
2023-05-23 15:37:22 +0800

    make it empty

--- src/utils/use-locale.tsx
+++
@@ -1,7 +0,0 @@
-import { useTranslation } from "next-i18next";
-
-export function useLocale(
-  namespace: Parameters<typeof useTranslation>[0] = "common"
-) {
-  return useTranslation(namespace)
-}
