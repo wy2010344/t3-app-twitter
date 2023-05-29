cafcc2f7b0708f1e611107f774d8279ea8077207
wyknmjj <wyknmjj@protonmail.com>
2023-05-29 13:56:57 +0800

    fix bug

--- app/hooks/useConversation.ts
+++ app/hooks/useConversation.ts
@@ -2,15 +2,18 @@ import { useParams } from "next/navigation";
 import { useMemo } from "react";
 
 
-
+/**
+ * 获得当前路径中的对话ID
+ * @returns 
+ */
 export default function useConversation() {
   const params = useParams()
   return useMemo(() => {
-    let conversationId = params.conversationId || ''
+    let conversationId = (params?.conversationId || '') as string
     const isOpen = !!conversationId
     return {
       isOpen,
       conversationId
     }
-  }, [params.conversationId])
+  }, [params?.conversationId])
 }
